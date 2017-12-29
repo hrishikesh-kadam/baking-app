7783b119f6ae8bd16495a4670742a95be1ddaeaa
Hrishikesh Kadam <hrkadam.92@gmail.com>
2017-12-29 17:27:46 +0530

    Add espresso test

--- app/src/androidTest/java/com/example/android/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/ExampleInstrumentedTest.java
@@ -1,25 +1,33 @@
 package com.example.android.bakingapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.JsonReader;
+import android.util.Log;
 
 import com.example.android.bakingapp.model.Recipe;
-import com.example.android.bakingapp.rest.RetrofitSingleton;
-import com.example.android.bakingapp.rest.WebServices;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-import retrofit2.Call;
-import retrofit2.Response;
-
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -30,6 +38,12 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
+    public static final String LOG_TAG = ExampleInstrumentedTest.class.getSimpleName();
+
+    @Rule
+    public ActivityTestRule<DetailsActivity> activityTestRule =
+            new ActivityTestRule<>(DetailsActivity.class, false, false);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -39,22 +53,45 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
-    public void checkDownloadedJson() throws Exception {
-
-        WebServices webServices = RetrofitSingleton.getRetrofit().create(WebServices.class);
-        Call<ArrayList<Recipe>> recipeCall = webServices.getAllRecipes();
-        Response<ArrayList<Recipe>> recipeResponse = null;
-
-        try {
-            recipeResponse = recipeCall.execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void testIfDetailsFragmentHidden() {
 
         Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader("E:\\Study Material\\Udacity\\Android Developer Nanodegree\\Projects\\BakingApp\\app\\src\\test\\java\\com\\example\\android\\bakingapp\\expectedResponse.json"));
-        System.out.println(reader.toString());
+        ArrayList<Recipe> recipeArrayList = null;
+        InputStream inputStream;
 
-        //System.out.println(recipeResponse.body());
+        try {
+            inputStream =
+                    InstrumentationRegistry.getContext().getAssets().open("expectedResponse.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Type type = new TypeToken<ArrayList<Recipe>>() {
+        }.getType();
+        recipeArrayList = gson.fromJson(new InputStreamReader(inputStream), type);
+
+        Intent intent = new Intent();
+        intent.putExtra("recipe", recipeArrayList.get(0));
+        activityTestRule.launchActivity(intent);
+
+        boolean isDualPane;
+
+        try {
+
+            onView(withId(R.id.guideline)).check(matches(isEnabled()));
+            isDualPane = true;
+
+        } catch (NoMatchingViewException e) {
+
+            isDualPane = false;
+        }
+
+        Log.i(LOG_TAG, "-> testIfDetailsFragmentHidden -> isDualPane = " + isDualPane);
+
+        if (!isDualPane) {
+            onView(withId(R.id.fragment_recipe_step_details)).check(matches(not(isDisplayed())));
+            Log.i(LOG_TAG, "-> testIfDetailsFragmentHidden -> fragment_recipe_step_details hidden on mobile UI");
+        }
     }
 }
